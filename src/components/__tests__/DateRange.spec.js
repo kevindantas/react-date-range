fcd5327fc9b8e6c71dabd3a1be56e2dad7fe71af
Kevin Dantas <kevinds29@gmail.com>
2019-03-21 15:52:34 -0600

    Add test when selecting endDate before startDate

--- src/components/__tests__/DateRange.spec.js
+++ src/components/__tests__/DateRange.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getAllByTestId, getByTestId, fireEvent } from 'react-testing-library';
+import { render, getByTestId, fireEvent } from 'react-testing-library';
 import DateRange from '../DateRange';
 
 const getByTestIdAndText = (container, testId, text) =>
@@ -10,26 +10,51 @@ const getByTestIdAndText = (container, testId, text) =>
 const getDayByText = (container, day) => getByTestIdAndText(container, 'day', day);
 
 describe('The DateRange Component', () => {
-  it('should select a range of days', () => {
-    let range = {
-      startDate: new Date(),
-      key: 'selection',
+  let range = {
+    startDate: new Date('2019-03-20'),
+    key: 'selection',
+  };
+  const handleChange = ({ selection }) => {
+    range = selection;
+  };
+
+  describe('when select range', () => {
+    const fireMouseEvents = (firstDay, secondDay) => {
+      fireEvent.mouseEnter(firstDay);
+      fireEvent.mouseDown(firstDay);
+      fireEvent.mouseMove(secondDay);
+      fireEvent.mouseUp(secondDay);
     };
-    const handleChange = ({ selection }) => {
-      range = selection;
-    };
-    const { container, rerender } = render(<DateRange ranges={[range]} onChange={handleChange} />);
 
-    const firstDay = getDayByText(container, '15');
-    const secondDay = getDayByText(container, '16');
-    fireEvent.mouseEnter(firstDay);
-    fireEvent.mouseDown(firstDay);
-    fireEvent.mouseMove(secondDay);
-    fireEvent.mouseUp(secondDay);
+    it('should be able to select a endDate after the startDate', () => {
+      const { container, rerender } = render(
+        <DateRange ranges={[range]} onChange={handleChange} />
+      );
 
-    rerender(<DateRange ranges={[range]} onChange={handleChange} />);
+      const firstDay = getDayByText(container, '15');
+      const secondDay = getDayByText(container, '16');
 
-    expect(firstDay).toContainElement(getByTestId(firstDay, 'in-range'));
-    expect(secondDay).toContainElement(getByTestId(secondDay, 'in-range'));
+      fireMouseEvents(firstDay, secondDay);
+
+      rerender(<DateRange ranges={[range]} onChange={handleChange} />);
+
+      expect(firstDay).toContainElement(getByTestId(firstDay, 'in-range'));
+      expect(secondDay).toContainElement(getByTestId(secondDay, 'in-range'));
+    });
+
+    it('should be able to select a endDate before the startDate', () => {
+      const { container, rerender } = render(
+        <DateRange ranges={[range]} onChange={handleChange} />
+      );
+
+      const firstDay = getDayByText(container, '15');
+      const secondDay = getDayByText(container, '14');
+      fireMouseEvents(firstDay, secondDay);
+
+      rerender(<DateRange ranges={[range]} onChange={handleChange} />);
+
+      expect(firstDay).toContainElement(getByTestId(firstDay, 'in-range'));
+      expect(secondDay).toContainElement(getByTestId(secondDay, 'in-range'));
+    });
   });
 });
