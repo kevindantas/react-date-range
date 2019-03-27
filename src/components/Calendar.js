ef25a4bee65181057e9c201f15bbed12cb8cb687
Kevin Dantas <kevinds29@gmail.com>
2019-03-27 13:13:33 -0600

    Add shadow to months with infinite scroll

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -30,8 +30,9 @@ import {
   DateDisplay,
   CalendarWrapper,
   Months,
-  InfiniteMonthsWrapper,
+  InfiniteMonthsList,
   DateDisplayItem,
+  InfiniteMonthsWrapper,
 } from './styles/Calendar.styled.js';
 import { WeekDays, WeekDay } from './styles/Month.styled.js';
 import MonthAndYearPicker from './MonthAndYearPicker.js';
@@ -319,9 +320,9 @@ class Calendar extends PureComponent {
         {showDateDisplay && this.renderDateDisplay()}
         {navigatorRenderer(focusedDate, this.changeShownDate, this.props)}
         {scroll.enabled ? (
-          <div>
+          <InfiniteMonthsWrapper>
             {isVertical && this.renderWeekdays(this.dateOptions)}
-            <InfiniteMonthsWrapper
+            <InfiniteMonthsList
               data-testid="infinite-months"
               direction={direction}
               onMouseLeave={() => onPreviewChange && onPreviewChange()}
@@ -370,8 +371,8 @@ class Calendar extends PureComponent {
                   );
                 }}
               />
-            </InfiniteMonthsWrapper>
-          </div>
+            </InfiniteMonthsList>
+          </InfiniteMonthsWrapper>
         ) : (
           <Months direction={direction}>
             {months.map((monthStep, i) => (
