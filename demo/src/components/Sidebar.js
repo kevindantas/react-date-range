63602da7d096c6643a08cb13130756ce42e84dc6
Kevin Dantas <kevinds29@gmail.com>
2019-03-28 10:30:59 -0600

    Add Calendar link and reorder the links

--- demo/src/components/Sidebar.js
+++ demo/src/components/Sidebar.js
@@ -5,7 +5,7 @@ import Alfredo from './Alfredo';
 const Aside = styled.aside`
   background: #fff;
   grid-area: sidebar;
-  width: 20vw;
+  width: 15vw;
   height: 100vh;
   position: sticky;
   flex-basis: 20% 1 1;
@@ -29,23 +29,34 @@ const ListLink = styled.a`
   text-decoration: none;
   color: #333;
   font-weight: 400;
+  display: block;
+`;
+
+const Logo = styled(Alfredo)`
+  width: 50%;
+  height: 140px;
+  margin: 1rem;
 `;
 
 function Sidebar() {
   return (
     <Aside>
-      <Alfredo width="50%" height="140" />
+      <Logo />
       <List>
+        <ListItem>
+          <ListLink href="#calendar">Calendar</ListLink>
+        </ListItem>
+
         <ListItem>
           <ListLink href="#date-range">Date Range</ListLink>
         </ListItem>
 
         <ListItem>
-          <ListLink href="#date-range-picker">Date Range Picker</ListLink>
+          <ListLink href="#defined-ranges">Defined Range</ListLink>
         </ListItem>
 
         <ListItem>
-          <ListLink href="#defined-ranges">Defined Range</ListLink>
+          <ListLink href="#date-range-picker">Date Range Picker</ListLink>
         </ListItem>
       </List>
     </Aside>
